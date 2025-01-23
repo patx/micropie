a2c5f929df7df56de082580ec86f9e7da89183d2
patx <harrisonerd@gmail.com>
2025-01-22 22:49:29 -0500

    change setup.py and release v0.4 (added streaming support for wsgi_app)

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ Links
 from distutils.core import setup
 
 setup(name="MicroPie",
-    version="0.3",
+    version="0.4",
     description="A ultra micro web framework w/ Jinja2.",
     long_description=__doc__,
     author="Harrison Erd",
