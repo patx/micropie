2e7e077ceb87e94f8d195999a638364ba637d610
patx <harrisonerd@gmail.com>
2025-01-28 17:58:40 -0500

    update setup.py, v0.9

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ Links
 from distutils.core import setup
 
 setup(name="MicroPie",
-    version="0.8",
+    version="0.9",
     description="A ultra micro web framework w/ Jinja2.",
     long_description=__doc__,
     author="Harrison Erd",
